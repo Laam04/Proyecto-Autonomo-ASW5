d29bca4f3e69fe930db54c3121836d58aa7f97fe
Jillder <115592175+Jillder@users.noreply.github.com>
2024-10-10 12:01:17 -0500

    Delete Def_Clientes directory

--- Def_Clientes/api/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Cliente
-
-class ItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cliente
-        fields = '__all__'
--- Def_Clientes/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import CLienteList, ClienteDetail
-
-urlpatterns = [
-    path('clientes/', CLienteList.as_view(), name='Cliente-list'),
-    path('Clientes/<int:pk>/', ClienteDetail.as_view(), name='Cliente-detail'),
-]
