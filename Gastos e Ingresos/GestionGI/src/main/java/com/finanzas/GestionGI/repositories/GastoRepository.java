a5a03d13aa75003933860500ef4cd0cac8c19590
Laam04 <168656256+Laam04@users.noreply.github.com>
2024-10-10 10:38:48 -0500

    EliminacionPA

--- Gastos e Ingresos/GestionGI/src/main/java/com/finanzas/GestionGI/repositories/GastoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.finanzas.GestionGI.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.finanzas.GestionGI.models.Gasto;
-
-public interface GastoRepository extends JpaRepository<Gasto, Long> {
-}
--- Gastos e Ingresos/GestionGI/src/main/java/com/finanzas/GestionGI/repositories/IngresoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.finanzas.GestionGI.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.finanzas.GestionGI.models.Ingreso;
-
-public interface IngresoRepository extends JpaRepository<Ingreso, Long> {
-}
