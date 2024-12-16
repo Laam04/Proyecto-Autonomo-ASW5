0db9309aa6034098d1765872cff83237ef643544
Laam04 <168656256+Laam04@users.noreply.github.com>
2024-12-16 13:33:27 -0500

    Delete gig

--- Proyecto Autonomo 2P/gestor-ingresos-gastos/src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return '¡Bienvenido al simulador de finanzas personales!';
-  }
-}
